904d9cd7b9a139808abf6fa93bd7fb093f112a7b
yankj12 <yankj12@163.com>
2019-02-15 11:33:39 +0800

    只保留easyui的上传方式，移除其他上传方式

--- src/main/resources/static/ajaxUpload.js
+++ src/main/resources/static/ajaxUpload.js
@@ -1,33 +1,5 @@
-function submit2(){ 
-	var category = $("#category").val();
-	var userCode = $("#userCode").val();
-	var fileNewName = $("#fileNewName").val();
 	
-	var type = "file"; 
-	//后台接收时需要的参数名称，自定义即可 
-	var id = "cert"; 
-	//即input的id，用来寻找值 
-	var formData = new FormData(); 
-	formData.append(type, $("#"+id)[0].files[0]); //生成一对表单属性 
-	
-	console.log($("#"+id)[0].files[0]);
-	
-	$.ajax({ 
-		type: "POST", //因为是传输文件，所以必须是post 
-		url: '/ajaxupload?userCode=' + userCode + '&category=' + category + "&fileNewName=" + fileNewName, //对应的后台处理类的地址 
-		data: formData, 
-		processData: false, 
-		contentType: false, 
-		success: function (result) { 
-			alert(result); 
-		},
-		failure:function (result) {  
-			alert(result);
-		}
-	}); 
-}
-	
-function submit3(){
+function submit(){
 	var userCode = $('#userCode_edit').textbox('getValue');
 	var category = $('#category_edit').textbox('getValue');
 	var fileName = $('#file_edit').filebox('getValue');
